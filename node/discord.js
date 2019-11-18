e58d797d9564e51d9f4a5309b7c375089a8512b5
Satnam Singh Brar <brar.satnam1995@gmail.com>
2019-11-17 19:06:13 -0800

    Added code to merge back received split messages
    - Also tried to handle the case where if a split ends with a space
    character then discord strings the space out before it reaches the other
    side. So, to preserve the space I have decided to replace it with a '`'
    character and hope that a split does not end up having an
    intentional '`' at the end.

--- node/discord.js
+++ node/discord.js
@@ -3,6 +3,12 @@ const fetch = require('node-fetch');
 const _ = require('lodash');
 const debug = require('debug')('messaging:discord');
 
+// GLOBALS ----------------------------------------------------------------------------------------
+
+const SESSION_MESSaGES_TIMEOUT = 60 * 1000; // 60 seconds
+
+sessions = {};
+
 // HELPERS ----------------------------------------------------------------------------------------
 
 function splitMessage(id, msg, charLimit) {
@@ -22,8 +28,13 @@ function splitMessage(id, msg, charLimit) {
 
     let msgSplit = msg.match(new RegExp(`.{1,${msgPieceMaxSize}}`, 'g'));
     msgSplit = _.map(msgSplit, (split, i) => {
+        // Discord strips out ending spaces, so we will replace them with a '`' character and hope
+        // that the split does not end with an intentional '`' character
+        split = split.replace(/\s+$/g, '`');
+
         return `${id},${i},${msgSplit.length}|${split}`;
     });
+
 
     console.log(msgSplit);
     return msgSplit;
@@ -41,6 +52,57 @@ async function sendDiscordMessage(msg, channelId, auth) {
         }
     });
 }
+
+function processSession(id, split, splitIndex, splitCount) {
+    /*
+    Returns false if the splitCount has not been fulfilled yet with this new split
+        - Adds the session data to `sessions` array
+    Returns the joined message if the splitCount has been satisfied
+    */
+
+    if (sessions[id]) {
+        if (_.size(sessions[id].splits) >= splitCount - 1) {
+            // We got the final split. Merge them all and delete the session object
+
+            sessions[id].splits[splitIndex] = split;
+
+            let fullMessage = '';
+            _.each(_(sessions[id].splits).keys().sortBy().value(), (index) => {
+                fullMessage += sessions[id].splits[index].toString();
+            });
+
+            delete sessions[id];
+
+            console.log('a', fullMessage);
+            return fullMessage;
+        } else {
+            // We still need more splits
+            sessions[id].splits[splitIndex] = split;
+
+            console.log('b');
+        }
+    } else {
+        if (splitCount === 1) {
+            // Do not need to do anything special because this is the one and only split we expect
+            console.log('c');
+            return split
+        } else {
+            // Start a new session
+            sessions[id] = {
+                splitCount: splitCount,
+                splits: {}
+            }
+
+            // Add the split at the split index
+            sessions[id].splits[splitIndex] = split;
+            console.log('d');
+        }
+    }
+
+    console.log('e');
+    return false;
+}
+
 // MODULE CODE ------------------------------------------------------------------------------------
 
 module.exports.isListening = false;
@@ -94,10 +156,30 @@ module.exports.startListening = (callbackReady, callbackMessage) => {
             debug('Discord message received from server');
             debug(msg.content)
 
+            // Deconstruct the information out of the message
+            const regexGroups = /(\d+),(\d+),(\d+)\|(.*)/g.exec(msg.content);
+            const id = parseInt(regexGroups[1]);
+            const splitIndex = parseInt(regexGroups[2]);
+            const splitCount = parseInt(regexGroups[3]);
+            const split = regexGroups[4];
+
+            // Convert any '`' at the end of the split to a ' '. But there may be more than 1 '`'
+            // so we need to replace them all with just as many ' '
+            endingSpaces = /`+$/g.exec(split);
+            if (endingSpaces) {
+                split.replace(/`+$/g, endingSpaces[1].replace('`', ' '));
+            }
+
+            const splitMerge = processSession(id, split, splitIndex, splitCount);
+            if (!splitMerge) {
+                return;
+            }
+
+            console.log(splitMerge);
             if (module.exports.isListening) {
                 debug('Sending message to callback');
-                callbackMessage(msg.content, (response, onSuccess) => {
-                    module.exports.sendMessage(response, onSuccess);
+                callbackMessage(splitMerge, (response, onSuccess) => {
+                    module.exports.sendMessage(response, onSuccess, id);
                 });
             } else {
                 debug('Listening has been stopped');
